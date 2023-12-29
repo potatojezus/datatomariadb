b0e42281f8bd558a14585f1fe72ddf5475dc5f72
lentilgod <lentilgod+github@gmail.com>
2023-12-29 11:20:47 +0100

    added main script

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python 
+import mariadb 
+
+conn = mariadb.connect(
+    user="pcpy",
+    password="*NID.M@j0wc9pInCH",
+    host="192.168.1.115",
+    port=3306,
+    database="Test1")
+
+cur = conn.cursor() 
+
+    
+#insert information 
+try: 
+    cur.execute("INSERT INTO TestTable1 (TestCol1, TestCol2, TestCol3, TestCol4) VALUES (?, ?, ?, ?)", ("Maria", 1, 2, 3)) 
+except mariadb.Error as e: 
+    print(f"Error: {e}")
+
+conn.commit() 
+print(f"Last Inserted ID: {cur.lastrowid}")
+    
+conn.close()
