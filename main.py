c0b613054a67a196fa39f9c6b9514fa4322cc921
lentilgod <lentilgod+github@gmail.com>
2023-12-29 11:47:17 +0100

    another change

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ cur = conn.cursor()
     
 #insert information 
 try: 
-    cur.execute("INSERT INTO TestTable1 (TestCol1, TestCol2, TestCol3, TestCol4) VALUES (?, ?, ?, ?)", ("Maria", 2, 2, 2)) 
+    cur.execute("""INSERT INTO TestTable1 (TestCol1, TestCol2, TestCol3, TestCol4) 
+                    VALUES (?, ?, ?, ?)""", ("Maria", 3, 2, 2)) 
 except mariadb.Error as e: 
     print(f"Error: {e}")
 
